7d91206aba60421f1e8f7c33d7ac7f105467e9aa
PedroacBraz <pedrobraz@comp.ufu.br>
2016-11-17 18:20:25 -0200

    Add project files.

---
+++ MarsRovers/MarsRovers.cs
@@ -0,0 +1,77 @@
+﻿#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRovers
+{
+    class MarsRovers
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Lista para armazenar os rovers criados
+            //List<Rover> myRovers = new List<Rover>();
+
+            Rover myRover;
+
+            String coords;
+            String roverCoords;
+            int upperLimitX, upperLimitY;
+
+            Char delimiter = ' ';
+
+            // Recebe as coordenadas delimitantes
+            Console.WriteLine("Inform the upper right coords of the plateau (Example format: 4 7): ");
+
+            coords = Console.ReadLine();
+
+            String[] substrings = coords.Split(delimiter);
+
+            upperLimitX = int.Parse(substrings[0]);
+            upperLimitY = int.Parse(substrings[1]);
+
+            //Start roover setup
+
+           while (true) {
+
+                Console.WriteLine("Insert the Rover's position and orientation (use position -1 -1 to finish rover input)");
+                roverCoords = Console.ReadLine();
+                //Divide a linha recebida e converte as substrings nos tipos necessários durante a criação do novo Rover
+                //adicionando-o na lista de Rovers criados
+                substrings = roverCoords.Split(delimiter);
+                //myRovers.Add(new Rover(int.Parse(substrings[0]), int.Parse(substrings[1]), char.Parse(substrings[2])));
+                myRover = new Rover(int.Parse(substrings[0]), int.Parse(substrings[1]), char.Parse(substrings[2]), upperLimitX, upperLimitY);
+
+#if DEBUG       
+                /*
+                Console.WriteLine("New rover in position " + myRovers[myRovers.Count - 1].getX() 
+                    + " " + myRovers[myRovers.Count - 1].getY()
+                    + " " + myRovers[myRovers.Count - 1].getDir());
+
+                */
+
+                Console.WriteLine("New rover in position " + myRover.getX()
+                    + " " + myRover.getY()
+                    + " " + myRover.getDir());
+
+#endif
+
+
+                Console.WriteLine("Please inform the rover's control string: ");
+                myRover.setCtrlString (Console.ReadLine());
+                myRover.executeCtrl();
+                
+
+            }
+
+
+
+            Console.ReadLine();
+            return;
+        }
+    }
+}
---
+++ MarsRovers/Rover.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRovers
+{
+    class Rover
+    {
+
+        int X;
+        int Y;
+        char dir;
+        int upperLimitX;
+        int upperLimitY;
+        String controlString;
+
+
+        //inicializa o Rover
+        public Rover(int X, int Y, char dir, int upperLimitX, int upperLimitY)
+        {
+
+            this.X = X;
+            this.Y = Y;
+            this.dir = dir;
+            this.upperLimitX = upperLimitX;
+            this.upperLimitY = upperLimitY;
+
+        }
+
+        //Vira à esquerda
+        public void turnLeft()
+        {
+
+            if (this.dir == 'N')
+            {
+                this.dir = 'W';
+            }
+            else if (this.dir == 'W')
+            {
+                this.dir = 'S';
+            }
+            else if (this.dir == 'S')
+            {
+                this.dir = 'E';
+            }
+            else if (this.dir == 'E')
+            {
+                this.dir = 'N';
+            }
+        }
+
+        // Vira à direita
+        public void turnRight()
+        {
+
+            if (this.dir == 'N')
+            {
+                this.dir = 'E';
+            }
+            else if (this.dir == 'E')
+            {
+                this.dir = 'S';
+            }
+            else if (this.dir == 'S')
+            {
+                this.dir = 'W';
+            }
+            else if (this.dir == 'W')
+            {
+                this.dir = 'N';
+            }
+        }
+
+        // Move à frente caso condições necessárias forem atendidas
+        public void moveFwrd()
+        {
+            if ((this.dir == 'N') && (this.Y < upperLimitY))
+            {
+                this.Y++;
+            }
+            else if ((this.dir == 'W') && (this.X > 0))
+            {
+                this.X--;
+            }
+            else if ((this.dir == 'S') && (this.Y > 0)) {
+                this.Y--;
+            }
+            else if ((this.dir == 'E') && (this.X < upperLimitX)){
+                this.X++;
+            }
+
+
+        }
+
+        // Recebe a String de instruções e movimenta o Rover
+        public void executeCtrl()
+        {
+
+            for (int i = 0; i < this.controlString.Length; i++)
+            {
+
+                if (controlString[i] == 'L')
+                {
+                    turnLeft();
+                }
+                else if (controlString[i] == 'R')
+                {
+                    turnRight();
+                }
+                else if (controlString[i] == 'M')
+                {
+                    moveFwrd();
+                }
+
+            }
+        }
+
+
+
+        public int getX()
+        {
+            return this.X;
+        }
+
+        public int getY()
+        {
+            return this.Y;
+        }
+
+        public char getDir()
+        {
+            return this.dir;
+        }
+
+        public void setCtrlString(String ctrlString)
+        {
+            this.controlString = ctrlString;
+        }
+
+        public String getCtrlString()
+        {
+            return this.controlString;
+        }
+    }
+}
